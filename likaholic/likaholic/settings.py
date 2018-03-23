76007d1fb50c08e59ab490e2832a669edb7b781f
pvn-quyen-le <quyen.le@punch.vn>
2018-03-23 11:53:28 +0700

    New project: token

--- likaholic/likaholic/settings.py
+++ likaholic/likaholic/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'tokens',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -74,10 +75,21 @@ WSGI_APPLICATION = 'likaholic.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'likaholic',
+        'HOST': 'localhost',
+        'PORT': 3306,
+        'USER': 'likaholic',
+        'PASSWORD': '123456a@A',
+        'OPTIONS': {
+        },
     }
+
 }
 
 
